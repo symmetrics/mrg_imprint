eb4498ae0a678094b989f58e77e1462e5de87d48
Siegfried Schmitz <ss@symmetrics.de>
2010-09-22 16:29:44 +0200

    EASY-37: Fixed bug so that mails there were send from admin (like invoice mails) take`s the impressum data from the correct StoreView and not always from the default one.

--- build/app/code/community/Symmetrics/Imprint/Block/Abstract.php
+++ build/app/code/community/Symmetrics/Imprint/Block/Abstract.php
@@ -28,6 +28,7 @@
  * @package   Symmetrics_Imprint
  * @author    symmetrics gmbh <info@symmetrics.de>
  * @author    Sergej Braznikov <sb@symmetrics.de>
+ * @author    Siegfried Schmitz <ss@symmetrics.de>
  * @copyright 2010 symmetrics gmbh
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @link      http://www.symmetrics.de/
@@ -35,12 +36,41 @@
 class Symmetrics_Imprint_Block_Abstract extends Mage_Core_Block_Template
 {
     /**
-     * Constructor
+     * Set StoreId to get impressum data for this store.
+     *
+     * @param int $storeId Store id.
+     *
+     * @return void
+     */
+    public function setStoreId($storeId) 
+    {
+        $this->setData(Mage::getStoreConfig('general/impressum', $storeId));
+    }
+
+    /**
+     * Getting StoreId to get proper store related
+     * information in order comments.
+     *
+     * @return int|null
+     */
+    protected function getStoreId()
+    {
+        $orderId = $this->getRequest()->getParam('order_id', 0);
+
+        if ($orderId > 0) {
+            return Mage::getSingleton('sales/order')->load($orderId)->getStoreId();
+        }
+        return null;
+    }
+    
+    /**
+     * Constructor to set config store view.
      *
      * @return void
      */
     public function __construct()
     {
-        $this->setData(Mage::getStoreConfig('general/imprint', $this->getStoreId()));
+        $storeId = $this->getStoreId();
+        $this->setData(Mage::getStoreConfig('general/imprint', $storeId));
     }
 }
